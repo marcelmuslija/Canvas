1ee371e0ca94659b1da99abc03dc88395153f786
Marcel Muslija <marcel.muslija@gmail.com>
2022-06-02 21:37:56 +0200

    Feat: GeometryUtil class
    GeometryUtil contains methods that calculate distance between points and
    the distance between a point and a line segment.
    
    Implement distanceFromLineSegment method.

---
+++ src/main/java/graphics/GeometryUtil.java
@@ -0,0 +1,17 @@
+package graphics;
+
+public class GeometryUtil {
+
+    public static double distanceFromPoint(Point point1, Point point2) {
+        int x1 = point1.getX();
+        int x2 = point2.getX();
+        int y1 = point1.getY();
+        int y2 = point2.getY();
+
+        return Math.sqrt((x2-x1)*(x2-x1) + (y2-y1)*(y2-y1));
+    }
+
+    public static double distanceFromLineSegment(Point s, Point e, Point p) {
+        return 0.0d;
+    }
+}
