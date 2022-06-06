b359636ee57b5e7ef774528ef2d3d4cb6b5c598e
Marcel Muslija <marcel.muslija@gmail.com>
2022-06-07 00:25:38 +0200

    Feat: State interface and IdleState

---
+++ src/main/java/states/IdleState.java
@@ -0,0 +1,43 @@
+package states;
+
+import graphics.geometry.Point;
+import graphics.objects.GraphicalObject;
+import graphics.rendering.Renderer;
+
+public class IdleState implements State {
+
+    @Override
+    public void mouseDown(Point mousePoint, boolean shiftDown, boolean ctrlDown) {
+
+    }
+
+    @Override
+    public void mouseUp(Point mousePoint, boolean shiftDown, boolean ctrlDown) {
+
+    }
+
+    @Override
+    public void mouseDragged(Point mousePoint) {
+
+    }
+
+    @Override
+    public void keyPressed(int keyCode) {
+
+    }
+
+    @Override
+    public void afterDraw(Renderer r, GraphicalObject go) {
+
+    }
+
+    @Override
+    public void afterDraw(Renderer r) {
+
+    }
+
+    @Override
+    public void onLeaving() {
+
+    }
+}
---
+++ src/main/java/states/State.java
@@ -0,0 +1,18 @@
+package states;
+
+import graphics.geometry.Point;
+import graphics.objects.GraphicalObject;
+import graphics.rendering.Renderer;
+
+public interface State {
+
+    void mouseDown(Point mousePoint, boolean shiftDown, boolean ctrlDown);
+    void mouseUp(Point mousePoint, boolean shiftDown, boolean ctrlDown);
+    void mouseDragged(Point mousePoint);
+    void keyPressed(int keyCode);
+
+    void afterDraw(Renderer r, GraphicalObject go);
+    void afterDraw(Renderer r);
+
+    void onLeaving();
+}
