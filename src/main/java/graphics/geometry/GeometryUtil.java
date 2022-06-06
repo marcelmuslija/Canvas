9c28bf07b80e0f5e42d5f2d6b6642fb565f2c9a0
Marcel Muslija <marcel.muslija@gmail.com>
2022-06-06 22:20:14 +0200

    Update: Implemented GeometryUtil#distanceFromLineSegment

--- src/main/java/graphics/geometry/GeometryUtil.java
+++ src/main/java/graphics/geometry/GeometryUtil.java
@@ -11,7 +11,30 @@ public class GeometryUtil {
         return Math.sqrt((x2-x1)*(x2-x1) + (y2-y1)*(y2-y1));
     }
 
+    /**
+     *
+     * <p>The line extending the line segment is parametrized as f(t) = s + t(e - s).
+     * The projection of the target point onto the line falls where t = dot(p - s, e - s).</p>
+     * Clamping t in range [0, 1] handles points outside the line segment.
+     *
+     * @param s start point of line segment
+     * @param e end point of line segment
+     * @param p target point
+     * @return distance between the target point and the line segment
+     */
     public static double distanceFromLineSegment(Point s, Point e, Point p) {
-        return 0.0d;
+        double lineLength = distanceFromPoint(s, e);
+        if (lineLength == 0.0d)
+            return distanceFromPoint(s, p);
+
+        double t = Math.max(0d, Math.min(1d, dot(p.difference(s), e.difference(s))/(lineLength*lineLength)));
+        int projX = (int) (s.getX() + t*(e.getX() - s.getX()));
+        int projY = (int) (s.getY() + t*(e.getY() - s.getY()));
+        Point projection = new Point(projX, projY);
+        return distanceFromPoint(p, projection);
+    }
+
+    private static int dot(Point a, Point b) {
+        return a.getX()*b.getX() + a.getY()*b.getY();
     }
 }
