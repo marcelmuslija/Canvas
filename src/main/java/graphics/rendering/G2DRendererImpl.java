d089b60e0d6611ec69d7822333757a43aa24a8fb
Marcel Muslija <marcel.muslija@gmail.com>
2022-06-06 23:03:37 +0200

    Update: G2DRendererImpl methods implemented

--- src/main/java/graphics/rendering/G2DRendererImpl.java
+++ src/main/java/graphics/rendering/G2DRendererImpl.java
@@ -2,7 +2,9 @@ package graphics.rendering;
 
 import graphics.geometry.Point;
 
+import java.awt.Color;
 import java.awt.Graphics2D;
+import java.awt.Polygon;
 
 public class G2DRendererImpl implements Renderer {
     private final Graphics2D g2d;
@@ -13,11 +15,24 @@ public class G2DRendererImpl implements Renderer {
 
     @Override
     public void drawLine(Point s, Point e) {
-
+        g2d.setColor(Color.BLUE);
+        g2d.drawLine(s.getX(), s.getY(), e.getX(), e.getY());
     }
 
     @Override
     public void fillPolygon(Point[] points) {
+        int[] xPoints = new int[points.length];
+        int[] yPoints = new int[points.length];
 
+        for (int i = 0; i < points.length; i++) {
+            xPoints[i] = points[i].getX();
+            yPoints[i] = points[i].getY();
+        }
+
+        g2d.setColor(Color.BLUE);
+        g2d.fillPolygon(xPoints, yPoints, points.length);
+
+        g2d.setColor(Color.RED);
+        g2d.drawPolygon(xPoints, yPoints, points.length);
     }
 }
