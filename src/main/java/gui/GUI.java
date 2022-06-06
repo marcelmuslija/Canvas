8ee0130411d305d12ecc0d10fc5e9972873976b8
Marcel Muslija <marcel.muslija@gmail.com>
2022-06-06 22:43:32 +0200

    Update: Added toolbar with buttons for GO creation to GUI.

--- src/main/java/gui/GUI.java
+++ src/main/java/gui/GUI.java
@@ -1,17 +1,39 @@
 package gui;
 
-import javax.swing.JFrame;
-import javax.swing.SwingUtilities;
+import graphics.GraphicalObject;
+import graphics.geometry.LineSegment;
+import graphics.geometry.Oval;
+
+import javax.swing.*;
+import java.awt.BorderLayout;
+import java.awt.Container;
+import java.util.List;
 
 public class GUI extends JFrame {
 
-    public GUI() {
-        
+    private List<GraphicalObject> objects;
+
+    public GUI(List<GraphicalObject> objects) {
+        this.objects = objects;
+        setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
+        setSize(500, 500);
+        initGUI();
+    }
+
+    private void initGUI() {
+        Container cp = getContentPane();
+        cp.setLayout(new BorderLayout());
+
+        JToolBar toolBar = new JToolBar();
+        objects.forEach(go -> toolBar.add(new JButton(go.getShapeName())));
+
+        cp.add(toolBar, BorderLayout.PAGE_START);
     }
 
     public static void main(String[] args) {
+        List<GraphicalObject> gos = List.of(new Oval(), new LineSegment());
         SwingUtilities.invokeLater(() -> {
-            new GUI().setVisible(true);
+            new GUI(gos).setVisible(true);
         });
     }
 }
