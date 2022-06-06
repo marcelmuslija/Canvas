cfdf57ea77359867fad8eacc07a1fe949f4ee964
Marcel Muslija <marcel.muslija@gmail.com>
2022-06-06 19:35:13 +0200

    Feat: GraphicalObjectListener interface
    Observing  graphical object changes.

---
+++ src/main/java/graphics/GraphicalObjectListener.java
@@ -0,0 +1,8 @@
+package graphics;
+
+public interface GraphicalObjectListener {
+
+    void graphicalObjectChanged(GraphicalObject go);
+    void graphicalObjectSelectionChanged(GraphicalObject go);
+
+}
