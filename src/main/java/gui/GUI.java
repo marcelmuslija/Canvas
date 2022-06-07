be61cb6eec4bd0d95c07da7dc16b97c034a1b37a
Marcel Muslija <marcel.muslija@gmail.com>
2022-06-07 08:10:40 +0200

    Added button for loading

--- src/main/java/gui/GUI.java
+++ src/main/java/gui/GUI.java
@@ -1,5 +1,6 @@
 package gui;
 
+import graphics.objects.CompositeShape;
 import graphics.objects.GraphicalObject;
 import graphics.objects.LineSegment;
 import graphics.objects.Oval;
@@ -14,10 +15,16 @@ import java.awt.event.ActionEvent;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 public class GUI extends JFrame {
+    private static final Map<String, GraphicalObject> prototypes = new HashMap<>();
+
+    static {
+        prototypes.put("@LINE", new LineSegment());
+        prototypes.put("@OVAL", new Oval());
+        prototypes.put("@COMP", new CompositeShape());
+    }
 
     private final List<GraphicalObject> objects;
     private final DocumentModel model;
@@ -38,8 +45,68 @@ public class GUI extends JFrame {
         cp.setLayout(new BorderLayout());
 
         Canvas canvas = new Canvas(model, this);
-
         JToolBar toolBar = new JToolBar();
+
+        JButton loadButton = new JButton(new AbstractAction("Učitaj") {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String filename = askLoadFilename();
+                if (filename == null)
+                    return;
+
+                Stack<GraphicalObject> stack = new Stack<>();
+                try {
+                    List<String> rows = Files.readAllLines(Path.of(filename));
+                    for (String row : rows) {
+                        String id = row.substring(0, row.indexOf(" "));
+                        GraphicalObject prototype = prototypes.get(id);
+                        prototype.load(stack, row.substring(row.indexOf(" ")+1));
+                    }
+                    stack.forEach(model::addGraphicalObject);
+                } catch (IOException ex) {
+                    showLoadError(filename);
+                }
+            }
+        });
+        toolBar.add(loadButton);
+
+        JButton saveButton = new JButton(new AbstractAction("Pohrani") {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String filename = askSaveFilename();
+                if (filename == null)
+                    return;
+                List<String> rows = new ArrayList<>();
+                model.list().forEach(go -> go.save(rows));
+                try {
+                    Files.writeString(Path.of(filename), String.join("\n", rows));
+                    showSaveSuccess();
+                } catch (Exception ex) {
+                    showSaveError(filename);
+                }
+            }
+        });
+        toolBar.add(saveButton);
+
+        JButton svgExportButton = new JButton(new AbstractAction("SVG Export") {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String filename = askSaveFilename();
+                if (filename == null)
+                    return;
+
+                SVGRendererImpl r = new SVGRendererImpl(filename);
+                model.list().forEach(go -> go.render(r));
+                try {
+                    r.close();
+                    showSaveSuccess();
+                } catch (Exception ex) {
+                    showSaveError(filename);
+                }
+            }
+        });
+        toolBar.add(svgExportButton);
+
         for (GraphicalObject go : objects) {
             JButton goButton = new JButton(new AbstractAction(go.getShapeName()) {
                 @Override
@@ -67,42 +134,6 @@ public class GUI extends JFrame {
         });
         toolBar.add(eraseButton);
 
-        JButton svgExportButton = new JButton(new AbstractAction("SVG Export") {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                String filename = askFilename();
-                if (filename == null)
-                    return;
-
-                SVGRendererImpl r = new SVGRendererImpl(filename);
-                model.list().forEach(go -> go.render(r));
-                try {
-                    r.close();
-                    showSuccess();
-                } catch (Exception ex) {
-                    showError(filename);
-                }
-            }
-        });
-        toolBar.add(svgExportButton);
-
-        JButton saveButton = new JButton(new AbstractAction("Pohrani") {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                String filename = askFilename();
-                if (filename == null)
-                    return;
-                List<String> rows = new ArrayList<>();
-                model.list().forEach(go -> go.save(rows));
-                try {
-                    Files.writeString(Path.of(filename), String.join("\n", rows));
-                    showSuccess();
-                } catch (Exception ex) {
-                    showError(filename);
-                }
-            }
-        });
-        toolBar.add(saveButton);
 
         cp.add(toolBar, BorderLayout.PAGE_START);
         cp.add(canvas, BorderLayout.CENTER);
@@ -117,13 +148,13 @@ public class GUI extends JFrame {
         currentState = state;
     }
 
-    private String askFilename() {
+    private String askSaveFilename() {
         JFileChooser fileChooser = new JFileChooser();
-        fileChooser.setDialogTitle("SVG Export");
-        if (fileChooser.showSaveDialog(GUI.this) != JFileChooser.APPROVE_OPTION) {
+        fileChooser.setDialogTitle("Spremi");
+        if (fileChooser.showSaveDialog(this) != JFileChooser.APPROVE_OPTION) {
             JOptionPane.showMessageDialog(
                     this,
-                    "Neuspješno stvaranje SVG datoteke!",
+                    "Neuspješno spremanje datoteke!",
                     "Upozorenje",
                     JOptionPane.WARNING_MESSAGE
             );
@@ -132,7 +163,22 @@ public class GUI extends JFrame {
         return fileChooser.getSelectedFile().toPath().toAbsolutePath().toString();
     }
 
-    private void showSuccess() {
+    private String askLoadFilename() {
+        JFileChooser fileChooser = new JFileChooser();
+        fileChooser.setDialogTitle("Učitaj");
+        if (fileChooser.showOpenDialog(this) != JFileChooser.APPROVE_OPTION) {
+            JOptionPane.showMessageDialog(
+                    this,
+                    "Neuspješno učitavanje datoteke!",
+                    "Upozorenje",
+                    JOptionPane.WARNING_MESSAGE
+            );
+            return null;
+        }
+        return fileChooser.getSelectedFile().toPath().toAbsolutePath().toString();
+    }
+
+    private void showSaveSuccess() {
         JOptionPane.showMessageDialog(
                 GUI.this,
                 "Datoteka spremljena!",
@@ -141,7 +187,7 @@ public class GUI extends JFrame {
         );
     }
 
-    private void showError(String filename) {
+    private void showSaveError(String filename) {
         JOptionPane.showMessageDialog(
                 GUI.this,
                 "Pogreška prilikom spremanja datoteke " + filename + ".\n" +
@@ -151,6 +197,14 @@ public class GUI extends JFrame {
         );
     }
 
+    private void showLoadError(String filename) {
+        JOptionPane.showMessageDialog(
+                GUI.this,
+                "Pogreška prilikom učitavanja datoteke " + filename + ".\n",
+                "Pogreška",
+                JOptionPane.ERROR_MESSAGE
+        );
+    }
 
     public static void main(String[] args) {
         List<GraphicalObject> gos = List.of(
