7c51ac6e2922f7c5b94a7ab3ece6f4885d1d089d
Marcel Muslija <marcel.muslija@gmail.com>
2022-06-07 07:34:25 +0200

    Added save button.

--- src/main/java/gui/GUI.java
+++ src/main/java/gui/GUI.java
@@ -12,7 +12,9 @@ import java.awt.BorderLayout;
 import java.awt.Container;
 import java.awt.event.ActionEvent;
 import java.io.IOException;
+import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.ArrayList;
 import java.util.List;
 
 public class GUI extends JFrame {
@@ -83,6 +85,24 @@ public class GUI extends JFrame {
             }
         });
         toolBar.add(svgExportButton);
+
+        JButton saveButton = new JButton(new AbstractAction("Pohrani") {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String filename = askFilename();
+                if (filename == null)
+                    return;
+                List<String> rows = new ArrayList<>();
+                model.list().forEach(go -> go.save(rows));
+                try {
+                    Files.writeString(Path.of(filename), String.join("\n", rows));
+                    showSuccess();
+                } catch (Exception ex) {
+                    showError(filename);
+                }
+            }
+        });
+        toolBar.add(saveButton);
 
         cp.add(toolBar, BorderLayout.PAGE_START);
         cp.add(canvas, BorderLayout.CENTER);
